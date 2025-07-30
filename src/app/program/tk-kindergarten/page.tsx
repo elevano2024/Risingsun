086c25c2e8db811f0d557c43026b8b50f63959aa
Anurag <anurag@copods.co>
2025-07-31 01:30:32 +0530

    updated TK

---
+++ src/app/program/tk-kindergarten/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/common/MainLayout";
+
+const Component = () => {
+    return <Layout header="Transitional Kindergarten / Kindergarten">
+
+        <div>TK</div>
+    </Layout>
+}
+
+export default Component;
